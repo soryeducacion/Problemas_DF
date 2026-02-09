c17c5d603f440c14bc31861af7d5167e049f53a5
soryeducacion <educacion.sory@gmail.com>
2026-02-09 12:14:47 -0600

    Add files via upload

---
+++ esqueleto.c
@@ -0,0 +1,18 @@
+/* ***
+* Esqueleto.c - programa que suma dos números
+* ***/
+#include <stdio.h>
+
+int main() {
+    // Declaro variables
+    int a,b,c;
+    // Asigno valores a las variables
+    a = 3;
+    b = -5;
+
+
+    // Realizo la suma
+    c = a + b;
+    // Muestro el resultado
+    printf("La suma es igual a: %d\n",c);
+}
