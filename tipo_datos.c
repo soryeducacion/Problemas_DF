be953fedac20bef402e80c47e1e26ed035be3daf
soryeducacion <educacion.sory@gmail.com>
2026-02-10 07:57:56 -0600

    Add files via upload

---
+++ tipo_datos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  // Create variables
+  int numero = 5;            // numeros enteros positivos o negativos
+  float decimales = 5.99;     // numeros decimales despues del punto
+  char letra = 'D';         // solo almacena caracteres
+  double PI = 3.1419542485;  //
+
+  // impresion de variables
+  printf("%d\n", numero);
+  printf("%f\n", decimales);
+  printf("%c\n", letra);
+  printf("%lf\n", PI);
+  
+  return 0;
+}
