d6fa169a2931158efa574c03fb5e59c33c1d3bc6
soryeducacion <educacion.sory@gmail.com>
2026-02-13 07:34:59 -0600

    Create operadores_aritmetica.c

---
+++ operadores_aritmetica.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b = 10, c = 3;
+	
+	a = b + c;
+	
+	printf("**************** Operadores aritmeticos ****************\n");
+	printf("La suma es: %d\n", a);
+	printf("La resta es: %d\n", b - c);
+	printf("La multiplicacion es: %d\n", b * c);
+	printf("La division es: %d\n", b / c);
+	printf("El residuo es: %d\n", b % c);
+	
+	return 0;
+}
