e93c06647dcdb735d1e3804de585672a5cf5f86c
soryeducacion <educacion.sory@gmail.com>
2026-02-13 07:31:35 -0600

    Update tipo_datos.c

--- tipo_datos.c
+++ tipo_datos.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 int main() {
-  // Create variables
-  int numero;            // numeros enteros positivos o negativos
-  float decimales;     // numeros decimales despues del punto
-  char letra;         // solo almacena caracteres
-  double PI;  //
+  int x;
+  int edad_alumno;
+  int edad_maestros,edad1, edad_1;
+  int numero = 5;            // numeros enteros positivos o negativos
+  float Decimales = 4.99;     // numeros decimales despues del punto
+  char L = 'q';
+  char l = 'q';
+  char nombre[] = "Sofia hola";        // solo almacena caracteres
+  const double PI = 3.1415956214;  //
+
+  numero = -5;
+  edad_alumno = 88;
 
   // impresion de variables
   printf("%d\n", numero);
-  printf("%f\n", decimales);
-  printf("%c\n", letra);
+  printf("%f\n", Decimales);
+  printf("%c\n", l);
   printf("%lf\n", PI);
-  
+
   return 0;
 }
-
