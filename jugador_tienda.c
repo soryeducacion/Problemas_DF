e5feb07a8127dd0f923db86d5aaee4a59d628189
soryeducacion <educacion.sory@gmail.com>
2026-02-16 22:08:07 -0600

    Add files via upload

---
+++ jugador_tienda.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Un jugador tiene monedas y gana recompensas después de una batalla.
+    También quiere entrar a una mazmorra que pide cierto nivel.
+    Debes:
+    1. Calcular las monedas totales después de la recompensa
+    2. Mostrar la vida máxima del jugador (constante)
+    3. Verificar si puede entrar a la mazmorra.
+    */
+    const int VIDA_MAXIMA = 100;
+
+    int monedas = 40;
+    int recompensa = 15;
+    int enemigos = 3;
+
+    int nivel_jugador = 12;
+    int nivel_requerido = 10;
+
+    int recompensaTotal = ; // Calcula el total de recompensa
+
+    int monedas_totales = ;   //Calcula las monedas totales
+
+    int puede_entrar = ; // decide si puede entrar a la mazmorra
+
+    printf("Vida maxima: %d\n", VIDA_MAXIMA);
+    printf("Recompensa total: %d\n", recompensa_total);
+    printf("Monedas totales: %d\n", monedas_totales);
+    printf("Puede entrar a la mazmorra: %d\n", puede_entrar);
+
+    return 0;
+}
