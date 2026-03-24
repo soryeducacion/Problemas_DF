ab2fd2434d1ba89f423cef12ecbac9ff45f36316
soryeducacion <educacion.sory@gmail.com>
2026-03-23 23:09:05 -0600

    Update ciclos_1.c

--- Ciclos/ciclos_1.c
+++ Ciclos/ciclos_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int experiencia = 0;
+
+    // El caballero entrena hasta llegar a 5 puntos de experiencia
+    while (experiencia < 5) {
+        printf("Entrenando con la espada: ");
+        printf("experiencia = %d\n", experiencia);
+        experiencia++;
+    }
+
+    printf("¡El caballero está listo para la ");
+    printf("batalla!\n");
+
+    return 0;
+}
