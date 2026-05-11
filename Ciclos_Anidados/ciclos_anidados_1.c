abe119936e5dfceae80c8a3e54e83715d1356927
soryeducacion <educacion.sory@gmail.com>
2026-05-11 12:06:14 -0600

    Update ciclos_anidados_1.c

--- Ciclos_Anidados/ciclos_anidados_1.c
+++ Ciclos_Anidados/ciclos_anidados_1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+/*
+Desarrolla un programa en lenguaje C que realice lo siguiente:
+1. Solicite al usuario dos valores enteros:
+• N y B
+2. Mientras el valor de N sea menor que 50, el programa debe:
+    • Imprimir los números del 1 hasta N
+    • Calcular un valor auxiliar:
+    T = N + 5
+    • Utilizar un ciclo para aumentar el valor de N de B en B, hasta que N sea
+    mayor o igual que T
+    • Imprimir el nuevo valor de N
 
+    */
 int main() {
     int N, B, T=0;
 
