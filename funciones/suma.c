008ed340be7a653612ae950ff3777290fe76c388
soryeducacion <educacion.sory@gmail.com>
2026-06-01 07:19:10 -0600

    Update suma.c

--- funciones/suma.c
+++ funciones/suma.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, total;
+
+    printf("Ingresa el primer numero: ");
+    scanf("%d", &num1);
+
+    printf("Ingresa el segundo numero: ");
+    scanf("%d", &num2);
+
+   // total = sumar(num1, num2);
+
+    printf("La suma es: %d\n", total);
+
+    return 0;
+}
+
+
+
+
